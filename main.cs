0d71613872bf3e8122592c952b73e19ef8f6d9a0
Frant24 <36748891+Frant24@users.noreply.github.com>
2023-09-01 21:30:31 +0000

    Test

--- main.cs
+++ main.cs
@@ -7,6 +7,6 @@ public class Program
         Console.WriteLine("Oh, well imagine /");
         Console.WriteLine("As I'm pacing the pews in a church corridor /");
         Console.WriteLine("And I can't help but to hear /");
-        Console.WriteLine("No, I can't help but to hear an exchanging of words..");
+        Console.WriteLine("No, I can't help but to hear an exchanging of words...");
     }
 }
