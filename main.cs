fdc0d7342875f941be83a5f0ab48e5cda2ffe5f9
Frant24 <36748891+Frant24@users.noreply.github.com>
2023-09-01 21:22:21 +0000

    Working piece of code that matches example outcome on canvas

--- main.cs
+++ main.cs
@@ -1,7 +1,12 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Oh, well imagine /");
+        Console.WriteLine("As I'm pacing the pews in a church corridor /");
+        Console.WriteLine("And I can't help but to hear /");
+        Console.WriteLine("No, I can't help but to hear an exchanging of words...");
+    }
 }
